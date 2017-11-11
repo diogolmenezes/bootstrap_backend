cba185b32eaef5a3e9e58cecc481e59625e76540
Diogo Menezes <diogolmenezes@gmail.com>
2017-11-11 15:32:21 -0200

    ajuste

--- app/modules/fatura/test/integration/fatura-controller.integration.js
+++ app/modules/fatura/test/integration/fatura-controller.integration.js
@@ -3,6 +3,23 @@ const expect = require('chai').expect;
 const supertest = require('supertest');
 
 describe('Fatura', () => {
+
+    after(() => {
+        // termina o processo do restify após todos os testes
+        process.exit();
+    })
+
+    it('Deve retornar uma fatura', (done) => {
+        supertest(server)
+            .get('/minha-aplicacao/cliente/1/fatura/12/2017')
+            .expect('Content-Type', /json/)
+            .expect(200)
+            .end(function (err, res) {
+                if (err) throw err;
+                done();
+            });
+    });
+
     it('Deve retornar uma fatura', (done) => {
         supertest(server)
             .get('/minha-aplicacao/cliente/1/fatura/12/2017')
